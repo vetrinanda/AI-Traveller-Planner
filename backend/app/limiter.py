071a7fe3495884fe9d974acb2d20fc1997ef1692
Nanda Kumar <nk9219382@gmail.com>
2026-02-21 22:11:19 +0530

    feat: introduce rate limiting setup using slowapi

--- backend/app/limiter.py
+++ backend/app/limiter.py
@@ -2,3 +2,7 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 
 limiter = Limiter(key_func=get_remote_address)
+
+'''
+For the rate limiting to work, we need to set the it
+'''
